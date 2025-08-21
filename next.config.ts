9d2c135328f52f318952652f19738c9d6a10258c
SnamikMT <elita.maks.elita@mail.ru>
2025-08-21 09:56:48 +0300

    full

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,14 @@
-// next.config.ts
 import type { NextConfig } from "next";
-
-const repo = "OurCamps";
-const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
   output: "export",
   distDir: "out",
-
-  // у тебя уже есть:
-  basePath: isProd ? `/${repo}` : "",
-  assetPrefix: isProd ? `/${repo}/` : "",
-
   images: {
-    loader: "custom",
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     unoptimized: true,
   },
   transpilePackages: ["next-image-export-optimizer"],
   env: {
-    // --- добавь это:
-    NEXT_PUBLIC_BASE_PATH: isProd ? `/${repo}` : "",
-    // --- и оставь твои оптимайзеры как были
     nextImageExportOptimizer_imageFolderPath: "public/images",
     nextImageExportOptimizer_exportFolderPath: "out",
     nextImageExportOptimizer_quality: "75",
